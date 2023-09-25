ed1fdbc1b8eab886ec7ef61bb9fd27265a38de39
bclittleua <32556428+bclittleua@users.noreply.github.com>
2023-09-25 11:46:55 -0700

    Update and rename RPI-LDR-LED to RPI-LDR-LED.py

--- RPI-LDR-LED.py
+++ RPI-LDR-LED.py
@@ -5,11 +5,12 @@ import time
 import sys
 import os
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
+GPIO.setup(12,GPIO.OUT)
 
-#define the pin that goes to the circuit
-pin_to_circuit = 17
+#define the BOARD pin to read LDR from
+pin_to_circuit = 11
 
 def rc_time (pin_to_circuit):
     count = 0
@@ -26,10 +27,19 @@ def rc_time (pin_to_circuit):
 lightVal = rc_time(pin_to_circuit)
 
 def write_data():
-     with open("ldrdata", "a") as log:
+     with open("ldrdata.csv", "a") as log: #be sure to specify path to "ldrdata.csv", i.e. /home/pi/bin/ldrdata.csv
           log.write("{0},{1},{2}\n".format(strftime("%Y-%m-%d"),strftime("%H:%M:%S "), lightVal))
+
+def led_func():
+    ldr_thresh = 200 #calibrate this number
+    if lightVal >= ldr_thresh:
+        GPIO.output(12,GPIO.HIGH) #if value is greater than thresh, set LED to on
+    if lightVal <- ldr_thresh:
+        GPIO.output(12,GPIO.LOW) #if value is lower than thresh, set LED to off
 
     # Main loop
 while True:
     write_data()
+    led_func()
+    time.sleep(1)
     break
